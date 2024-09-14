61ac1863210d9c67dba914ec90a63c2768ca189c
Alexey Levchuk <bizlexx@gmail.com>
2024-09-14 23:27:01 +0200

    Updates

---
+++ precache-manifest.f8d3800a23e35a0c48f47224bbc70192.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3892bc1e8bfac57f37e4233d03f2bd65",
+    "url": "/drag-drop-diagram/index.html"
+  },
+  {
+    "revision": "e39f13b5daf69aaeff39",
+    "url": "/drag-drop-diagram/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7cefa27d73e5f7f23348",
+    "url": "/drag-drop-diagram/static/js/2.33a115b8.chunk.js"
+  },
+  {
+    "revision": "e39f13b5daf69aaeff39",
+    "url": "/drag-drop-diagram/static/js/main.07234f4a.chunk.js"
+  },
+  {
+    "revision": "9bfa1078ed2811ea0f21",
+    "url": "/drag-drop-diagram/static/js/runtime~main.8f466f60.js"
+  }
+]);
